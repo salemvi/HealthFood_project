4acc2ae15a02c28d8144f8e858dd35f496e0c4d4
Vincent <zamanovvr@gmail.com>
2023-07-04 23:35:35 +0300

    add class for menu

--- src/js/main.js
+++ src/js/main.js
@@ -143,9 +143,73 @@ window.addEventListener('DOMContentLoaded', () => { //
             }
         }
         window.addEventListener('scroll', showModalbyScrollBottom);
+// Создание класса для карточек
 
+        class MenuCard {
+            constructor(src, alt, title, descr, price, parentElement, ...classes) { 
+                this.src = src;
+                this.alt = alt;
+                this.title = title;
+                this.descr = descr;
+                this.price = price;
+                this.classes = classes; 
+                this.course = 80;
+                this.parent = document.querySelector(parentElement);
+                this.changeToRub();
+            }
+            changeToRub () {
+                this.price = this.price * this.course;
+            }
+            render() {
+                const element = document.createElement('div');
+                if (this.classes.length === 0) { 
+                    this.classes.push('menu__item'); // если забыли написать класс - ставим класс по умолчанию
+                    element.classList.add(this.classes[0]); // добавляем в element класс по умолчанию
+                } else {
+                    this.classes.forEach(className => element.classList.add(className)); // если не забыли, добавляем те классы, которые передали ниже в MenuCard()
+                
+                }
+                element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>
+                `;
+                this.parent.append(element);
+            }
+        }
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            8,
+            '.menu .container',
 
+        ).render();
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            12,
+            '.menu .container',
+            'menu__item',
 
+        ).render();
+        new MenuCard(
+            "img/tabs/post.jpg" ,
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            10,
+            '.menu .container',
+            'menu__item',
+        ).render();
 
 });
 
@@ -168,3 +232,15 @@ window.addEventListener('DOMContentLoaded', () => { //
 // alex.hello();
 // console.log(vadim, alex);
 
+// REST оператор
+// const log = function(a, b, ...rest) {
+//     console.log(a, b, rest);
+// }
+// log('basic', 'rest', 'opetator', 'usage', 'salem');
+
+// function calcOrDouble(number, basis = 2)  {
+//     console.log(number * basis);
+// }
+// calcOrDouble(3);
+
+ 
